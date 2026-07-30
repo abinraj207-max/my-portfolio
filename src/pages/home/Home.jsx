2d1c5559833eea56f49407c7596a4ec70aedc16a
abinraj207-max <abinraj207@gmail.com>
2026-07-30 09:16:31 +0530

    Remove laptop intro animation

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -9,14 +9,10 @@ import Testimonials from "../../sections/Testimonials";
 import Contact from "../../sections/Contact";
 import FooterCTA from "../../sections/FooterCTA";
 
-import LaptopScrollIntro from "../../components/common/LaptopScrollIntro/LaptopScrollIntro";
-
 const Home = () => {
   return (
     <main className="min-h-screen bg-[#050816] text-white overflow-x-hidden">
-      <LaptopScrollIntro>
-        <Hero />
-      </LaptopScrollIntro>
+      <Hero />
       <About />
       <Skills />
       <Projects />
